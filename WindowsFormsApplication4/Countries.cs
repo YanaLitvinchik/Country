720aabc7497e30f8dd225218023428e1ddb7d49c
Yana <litvinchikyana@ukr.net>
2018-05-24 16:48:01 +0300

    Add project files.

---
+++ WindowsFormsApplication4/Countries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication4
+{
+    class  Countries
+    {
+        public List<string> Cities { get; set; }
+        public string Country { get; set; }
+        public Countries(string country)
+        {
+            Cities = new List<string>();
+            Country = country;
+        }
+        public void Remove(string s)
+        {
+                Cities.Remove(s);
+        }
+
+    }
+}
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        List<Countries> countries = new List<Countries>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            countries.Add(new Countries("Ukraine"));
+            comboBox1.DisplayMember = "Country";
+            comboBox1.Items.AddRange(countries.ToArray());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            countries[0].Cities.Add(textBox1.Text);
+            Show();
+        }
+        public void Show()
+        {
+            //listBox1.Items.Clear();
+            foreach (var item in countries)
+            {                
+                 listBox1.Items.AddRange(item.Cities.ToArray());
+                 
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            countries[0].Cities.Remove(listBox1.Text);
+            Show();  
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items[listBox1.SelectedIndex] = textBox2.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
